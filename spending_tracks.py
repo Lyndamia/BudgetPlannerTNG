532e8eb8797c430e3070f7fb0dbe57c59b6778d6
jev178 <jevaanraj17@gmail.com>
2025-09-24 10:29:28 +0800

    add base structure for spending_tracks.py with imports and constants

---
+++ spending_tracks.py
@@ -0,0 +1,12 @@
+# spending_tracks.py
+
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from datetime import date, datetime
+import data_manager
+import calendar
+
+# Font
+LARGE_FONT = ("Arial", 12)
+BUTTON_FONT = ("Arial", 12)
